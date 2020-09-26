4c7f09f4a69c5c810364e3260a0f53a6c40a176e
Juicyjames <iceandjames@gmail.com>
2020-09-26 12:40:10 -0600

    added basic structure of website

---
+++ gatsby/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; Slick's Slicers {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SliceMaster() {
+  return (
+    <>
+      <p>Hey! I'm the Slicemaster page</p>
+    </>
+  );
+}
