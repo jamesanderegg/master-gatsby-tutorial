e4920a7f9ec0f14a823b87d1ce471faff9bd4562
Juicyjames <iceandjames@gmail.com>
2020-10-05 14:55:19 -0600

    serverless function work, generateOrder Email in regualar javascript without React

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,26 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+  <h2>Your Recent order for ${total}</h2>
+  <p>Your order will be ready in the next 20 minutes</p>
+  <ul>
+  ${order
+    .map(
+      (item) => `<li>
+    <img src="${item.thumbnail}" alt="${item.name}"/>
+    ${item.size} ${item.name} - ${item.price}
+  </li>`
+    )
+    .join('')}
+  </ul>
+  <p> Your total is <strong>$${total}</strong> due at pickup.</p>
+  <style>
+    ul {
+      list-style: none;
+    } {</style>
+  </div>`;
+}
 // create transport for node mailer
 
 const transporter = nodemailer.createTransport({
@@ -12,16 +33,36 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+  // validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+  // using for each will create another function scope
+  for (const field of requiredFields) {
+    console.log(`Checking ${field}`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! your missing the ${field} field)`,
+        }),
+      };
+    }
+  }
+
+  // send the email
+
+  // send the succerss or error message
   // test send an email
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@example.com>",
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}> orders@example.com`,
     subject: 'New Order!',
-    html: `<p> Your new Pizza order is here!</>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
   console.log(info);
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
