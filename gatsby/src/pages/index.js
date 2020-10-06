b32fd7b45db688b7344b9c4a0851efa7bd9d71f0
Juicyjames <iceandjames@gmail.com>
2020-10-06 13:43:10 -0600

    fetch live data client side to build out our home page.  2 components in our index home page will take in the data and build visual. we must client side fetch because gatsby does not expose graphql endpoint after build process. added endpoints to our sanity io api as well.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
-export default function HomePage() {
+// we cannont use gatsby to get data because there is no API after build time.
+// So we must use the sanity API, client side, graphql endpoint to fetch the data
+
+function CurrentlySlicing() {
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-    </>
+    <div>
+      <p>Currently slicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>Hotslices</p>
+    </div>
+  );
+}
+export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+export default function useLatestData() {
+  // hotslices
+  const [hotSlices, setHotSlices] = useState();
+  // slicemasters
+  const [sliceMasters, setSliceMasters] = useState();
+
+  // use a side effect to fetch data from sanity graphql endpoint
+  useEffect(function () {
+    // when the componet loads, fetch the data
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+            query {
+                StoreSettings(id: "downtown"){
+                    name
+                    slicemaster {
+                        name
+                    }
+                    hotSlices {
+                        name
+                    }
+                }
+            }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // CHECK FOR ERRORS
+        // set data to state
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSliceMasters(res.data.StoreSettings.sliceMaster);
+      });
+  }, []);
+  return { hotSlices, sliceMasters };
+}
